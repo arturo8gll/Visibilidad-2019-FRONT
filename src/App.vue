8f2a65c5db978ac55d02db9c89803b87a1d31033
arturo guerrero <arturo8gll@gmail.com>
2019-08-28 02:06:43 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="line"></div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="line">
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+img{
+  margin-top: 50px;
+  width: 400px;
+}
+.line{
+  width: 100%;
+  height: 35px;
+  background-color: #1A415D;
+  box-shadow:         0px 16px 16px 0px rgba(50, 50, 50, 0.19);
+}
+.footer{
+  width: 100%;
+  background-color: #1A415D;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="container">
+    <h1>Todo List</h1>
+    <br>
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Titulo</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Titulo de la Tarea">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Descripción</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Descripción de la tarea">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Importancia</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <button type="submit" class="btn btn-danger">Crear</button>
+        </form>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-md-12 flex">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Titulo tarea</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Importancia</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Terminada</a>
+            <a href="#" class="card-link">Borra</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h1 {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 50px;
+}
+.flex{
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.card{
+  margin: 20px 0;
+}
+a{
+  color: #E24656;
+}
+</style>
